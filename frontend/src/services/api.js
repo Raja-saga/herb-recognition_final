4c989a03fe629e145e53222b9b78604f899fe1fb
Raja-saga <rajaganesh18904@gmail.com>
2026-04-10 15:20:18 +0530

    backend api added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:3001/api";
+const API_BASE = "http://31.97.239.242/api";
 
 export const predictHerb = async (image, location) => {
   const fd = new FormData();
@@ -46,7 +46,7 @@ export const getCurrentLocation = () => {
   });
 };
 export async function getStateMapData(herb) {
-  const res = await fetch(`http://localhost:3001/api/state-map/${herb}`);
+  const res = await fetch(`http://31.97.239.242:3001/api/state-map/${herb}`);
   if (!res.ok) throw new Error("Failed to load state map data");
   return res.json();
 }
